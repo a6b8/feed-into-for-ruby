4f84cca001ba2440bd1f5a3143a0c2b97e05903d
a6b8 <andreasbanholzer@me.com>
2021-10-04 06:11:54 +0200

    v0.01

--- test/1.rb
+++ test/1.rb
@@ -1,1 +1,17 @@
-puts 'OK'
+require './lib/feed_into'
+require 'active_support/core_ext/hash/indifferent_access'
+
+
+channel = {
+    name: :blockchain,
+    sym: :web,
+    options: {},
+    regexs: [ [ /https:\/\/raw.githubusercontent.com/ ] ],
+    download: :general,
+    mining: :rss_one,
+    pre: [],
+    transform: nil,
+    post: [ :pre_titles ]
+}
+
+puts 'CHANNEL:'
