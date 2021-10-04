a5def71b762fb64821f89213ba899ff4cebb4a80
a6b8 <andreasbanholzer@me.com>
2021-10-04 06:47:40 +0200

    v0.01

--- test/1.rb
+++ test/1.rb
@@ -89,4 +89,43 @@ tests = {
 res = feed.analyse( item: tests[:single][:string_error] )
 puts "- single-error:\t\t\t#{!res[:success]}"
 
-puts feed
+res = feed.analyse( item: tests[:single][:string] )
+puts "- single:\t\t\t#{res[:success]}"
+
+res = feed.analyse( item: tests[:single][:cmd_incomplete] )[:result][:items][ 0 ][:title].class.to_s.eql? 'String'
+puts "- single incomplete:\t\t#{res}"
+
+res = feed.analyse( item: tests[:single][:cmd_complete] )[:result][:items][ 0 ][:title].class.to_s.eql? 'String'
+puts "- single cmd complete:\t\t#{res}"
+
+res = feed.analyse( item: tests[:single][:cmd_error] )
+puts "- single cmd error:\t\t#{!res[:success]}"
+
+res = feeds
+    .analyse( items: tests[:group][:string], silent: true )
+    .to_h()[:unknown][ 0 ][:result][:items][ 0 ][:title].class.eql? String
+puts "- group string:\t\t\t#{res}"
+
+res = feeds
+    .analyse( items: tests[:group][:string], silent: true )
+    .merge
+    .to_h()[:unknown].length == 40
+puts "- group string error:\t\t#{res}"
+
+res = feeds
+    .analyse( items: tests[:group][:cmds_incomplete], silent: true )
+    .merge
+    .to_h().keys.length == 2 # [:unknown].length == 40
+puts "- group cmds incomplete:\t#{res}"
+
+res = feeds
+    .analyse( items: tests[:group][:cmds_complete], silent: true )
+    .merge
+    .to_h()[:crypto].length  == 40
+puts "- group cmds complete:\t\t#{res}"
+
+res = feeds
+    .analyse( items: tests[:group][:cmds_error], silent: true )
+    .merge
+    .to_h()[:crypto].length == 0
+puts "- group cmds error:\t\t#{res}"
